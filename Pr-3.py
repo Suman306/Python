ead8e3cf9b21a12a8c4eceb00b55bf5fbf8ddfbe
Patel Sumankumar <20ce105@charusat.edu.in>
2022-01-27 18:39:01 +0530

    init

---
+++ Pr-3.py
@@ -0,0 +1,12 @@
+
+k=int(input())  # taking input for k
+s=map(int,input().split())  # input for room list
+s=sorted(s)  # sorting the list
+
+for i in range(len(s)):
+    if(i!=len(s)-1):
+      if(s[i]!=s[i-1] and s[i]!= s[i+1]):
+          print(s[i])
+          break;   
+    else:
+         print(s[i])
